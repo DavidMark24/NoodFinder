8242c41a54ffaa625fe1e4720bb3f109e147ba87
Megan Bryan <meganmbryan12@gmail.com>
2021-04-09 21:23:38 -0700

    Add routes to all pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Choice from "./pages/Choice";
+import Login from "./pages/Login";
+import Main from "./pages/Main";
+import SignUp from "./pages/SignUp";
+import Splash from "./pages/Splash";
+import User from "./pages/User";
 
 function App() {
   return (
-    <div>
-      <h1> App </h1>
-    </div>
+    <Router>
+      <div>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/choice" component={Choice} />
+        <Route path="/main" component={Main} />
+        <Route path="/splash" component={Splash} />
+        <Route path="/user" component={User} />
+      </div>
+    </Router>
   );
 }
 
