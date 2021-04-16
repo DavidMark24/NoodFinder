c76700091cf2219d6d85c0e523dfa3cee2ecce4d
Megan Bryan <meganmbryan12@gmail.com>
2021-04-15 19:03:49 -0700

    Add rounding to images

--- client/src/pages/Choice.js
+++ client/src/pages/Choice.js
@@ -30,7 +30,7 @@ function Choice() {
                 <div class="row">
                     <div onClick={event =>  window.location.href='/dinein'} class="col-md-5 col-sm-12 shadow-lg p-3 m-5 rounded tan_bg">
                         <div class="box light_brown_bg p-2 rounded">
-                            <img src={dineIn} width='100%' alt=""/>
+                            <img className="rounded" src={dineIn} width='100%' alt=""/>
                         </div>
                         <div class="logo">
                             <img class="mt-2 py-4" src={dineInLogo} width='90%' alt=""/>
@@ -38,7 +38,7 @@ function Choice() {
                     </div>
                     <div onClick={event =>  window.location.href='/eatout'} class="col-md-5 col-sm-12 shadow-lg p-3 m-4 my-5 rounded tan_bg">
                         <div class="box light_brown_bg p-2 rounded">
-                            <img src={eatOut} width='100%' alt=""/>
+                            <img className="rounded" src={eatOut} width='100%' alt=""/>
                         </div>
                         <div class="logo">
                             <img class="mt-2 py-4" src={eatOutLogo} width='90%' alt=""/>
