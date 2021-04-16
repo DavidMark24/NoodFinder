3837b136dbaa86b6ffd46bcb09f2cbdf99488981
Ankush <ankushchalla@gmail.com>
2021-04-15 21:31:44 -0700

    Refactored to DRY.

--- client/src/pages/DineIn.js
+++ client/src/pages/DineIn.js
@@ -1,45 +1,77 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import dineInLogo from "../images/dineInLogo.svg";
 import dineIn from "../images/dineInSquare.jpg";
 import rejectButton from "../images/rejectButton.svg";
 import acceptButton from "../images/acceptButton.svg";
 import Footer from "../components/Footer";
+import API from "../utils/Api";
 
-function DineIn() {
+function DineIn(props) {
+    const [allRecipes, setAllRecipes] = useState([]);
+    // const [currentRecipe, setCurrentRecipe] = useState(null);
+    const [recipeIndex, setRecipeIndex] = useState(0);
+
+    useEffect(() => {
+        API.getRandomRecipe("mexican", "lunch")
+            .then(response => {
+                let goodRecipes = response.data
+                    .map(getRecipeObject)
+                    .filter(hasNonzeroTime);
+                console.log("goodRecipes:", goodRecipes);
+                setAllRecipes(goodRecipes);
+            });
+    }, []);
+
+    const getRecipeObject = (elem) => elem.recipe;
+    const hasNonzeroTime = (recipe) => recipe.totalTime !== 0;
+
+    function changeRecipe(event) {
+        event.preventDefault();
+        setRecipeIndex(recipeIndex + 1);
+    }
+    console.log("allRecipes:", allRecipes);
+    console.log("current recipe:", allRecipes[recipeIndex]);
     return (
         <div>
-            <div className="jumbotron logo py-4">
-                <img src={dineInLogo} height='150' alt="Nood Finder Logo"/>
-            </div>
-            <div className="container logo">
-                    <div className="col-md-5 col-sm-12 shadow-lg p-3 m-4 my-5 extra-rounded tan_bg">
-                        <div className="box light_brown_bg p-2 rounded">
-                            <img className="rounded" src={dineIn} width='100%' alt=""/>
+            {
+                allRecipes[recipeIndex] == null ?
+                    <div></div>
+                    :
+                    <>
+                        <div className="jumbotron logo py-4">
+                            <img src={dineInLogo} height='150' alt="Nood Finder Logo" />
                         </div>
-                        <div className="p-3">
-                            <div className="row text-center">
-                                <h1><strong>Recipe Title</strong></h1>
-                            </div>
-                            <div className="row text-center">
-                                <h3>Preparation Time</h3>
-                            </div>
-                            <div className="row text-center">
-                                <h3>Dietary Restrictions</h3>
-                            </div>
-                            <div className="row text-center mt-4">
-                                <div className="col-4">
-                                    <img onClick={event => alert("Disliked")} src={rejectButton} width='100%' alt=""/>
+                        <div className="container logo">
+                            <div className="col-md-5 col-sm-12 shadow-lg p-3 m-4 my-5 extra-rounded tan_bg">
+                                <div className="box light_brown_bg p-2 rounded">
+                                    <img className="rounded" src={allRecipes[recipeIndex].image} width='100%' alt="" />
                                 </div>
-                                <div className="col-4">
-                                    {/* Empty column */}
-                                </div>
-                                <div className="col-4">
-                                    <img onClick={event => alert("Liked")} src={acceptButton} width='100%' alt=""/>
+                                <div className="p-3">
+                                    <div className="row text-center">
+                                        <h1><strong>{allRecipes[recipeIndex].label}</strong></h1>
+                                    </div>
+                                    <div className="row text-center">
+                                        <h3>Preparation Time: {allRecipes[recipeIndex].totalTime}</h3>
+                                    </div>
+                                    <div className="row text-center">
+                                        <h3>Dietary Restrictions</h3>
+                                    </div>
+                                    <div className="row text-center mt-4">
+                                        <div className="col-4">
+                                            <img onClick={event => alert("Disliked")} src={rejectButton} width='100%' alt="" />
+                                        </div>
+                                        <div className="col-4">
+                                            {/* Empty column */}
+                                        </div>
+                                        <div className="col-4">
+                                            <img onClick={event => alert("Liked")} src={acceptButton} width='100%' alt="" />
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-            </div>
+                    </>
+            }
             <Footer />
         </div>
     );
