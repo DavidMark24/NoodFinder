444269af0a377ddcd561b04899ecf0674c70f7df
Ankush <ankushchalla@gmail.com>
2021-04-17 13:49:45 -0700

    Added recipe url.

--- client/src/utils/Api.js
+++ client/src/utils/Api.js
@@ -8,6 +8,9 @@ const API = {
   getRandomRecipe: (genre, query) => {
     return axios.get(`/api/cuisine/${genre}/${query}`);
   },
+  addToFavorites: (recipe) => {
+    return axios.post(`/api/recipes`, recipe);
+  },
 // Yelp
   handleYelp: (searchTerms, searchLocation) => {
     return axios.get(
