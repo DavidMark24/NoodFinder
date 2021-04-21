d0b6fafdd31f1563b6337bd511201300de8a6631
Megan Bryan <meganmbryan12@gmail.com>
2021-04-20 20:45:57 -0700

    Change how links are set up

--- client/src/pages/Choice.js
+++ client/src/pages/Choice.js
@@ -9,32 +9,31 @@ import { Link } from "react-router-dom";
 
 
 function Choice() {
-
     return (
         <div>
             <OnlyFoodsLogo width="50%"/>
             <div class="container">
                 <div class="row logo">
-                    <Link to = "/dinein">
-                        <div class="col-lg-5 col-md-7 col-sm-12 shadow-lg p-3 m-5 rounded tan_bg">
-                            <div class="box light_brown_bg p-2 rounded">
-                                <img className="rounded" src={dineIn} width='100%' alt=""/>
-                            </div>
-                            <div class="logo">
-                                <img class="mt-2 py-4" src={dineInLogo} width='90%' alt=""/>
-                            </div>
+                    <div class="col-lg-5 col-md-5 col-sm-12 shadow-lg p-3 m-4 rounded tan_bg">
+                    <Link to = "/cuisines">
+                        <div class="box light_brown_bg p-2 rounded">
+                            <img className="rounded" src={dineIn} width='100%' alt=""/>
+                        </div>
+                        <div class="logo">
+                            <img class="mt-2 py-4" src={dineInLogo} width='90%' alt=""/>
                         </div>
                     </Link>
-                    <Link to = "/eatout">
-                        <div class="col-lg-5 col-md-7 col-sm-12 shadow-lg p-3 m-5 rounded tan_bg">
-                            <div class="box light_brown_bg p-2 rounded">
-                                <img className="rounded" src={eatOut} width='100%' alt=""/>
-                            </div>
-                            <div class="logo">
-                                <img class="mt-2 py-4" src={eatOutLogo} width='90%' alt=""/>
-                            </div>
+                    </div>
+                    <div class="col-lg-5 col-md-5 col-sm-12 shadow-lg p-3 m-4 rounded tan_bg">
+                    <Link to = "/cuisines">
+                        <div class="box light_brown_bg p-2 rounded">
+                            <img className="rounded" src={eatOut} width='100%' alt=""/>
+                        </div>
+                        <div class="logo">
+                            <img class="mt-2 py-4" src={eatOutLogo} width='90%' alt=""/>
                         </div>
                     </Link>
+                    </div>
                 </div>
             </div>
             <Footer />
