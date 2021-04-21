aa422db03c7bb72d4a7a62af535dc3545dd0aa12
David Johnson <david.m.johnson32@gmail.com>
2021-04-20 20:35:35 -0700

    Saved changes on yelp api

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -5,19 +5,32 @@ require('dotenv').config();
 
 const BASEURL = "https://api.yelp.com/v3/businesses/search"
 const BEARER = process.env.BEARERTOKEN
-
+// const config = {
+//     headers: {
+//         Authorization: `Bearer ${BEARER}`
+//     }
+// };
+// const secondAPICall = (id) => {
+//     axios.get(`https://api.yelp.com/v3/businesses/${id}`, config).then(results => results.data)
+// }
 // Yelp API
-router.get("/api/restaurantsearch/:term/:location" , (req , res) =>{
+router.get("/api/restaurantsearch/:term/:location" , (req , res) => {
     const config = {
         headers: {
             Authorization: `Bearer ${BEARER}`
         }
     };
     axios.get("https://api.yelp.com/v3/businesses/search?term=" + req.params.term + "&location=" + req.params.location +"&limit=50", config).then(results => {
-      
-   res.json(results.data.businesses)
+        res.json(results.data.businesses)
+    // console.log(results.data.businesses[0].id)
+     //res.json( secondAPICall(results.data.businesses[0].id)
+    //    const id = results.data.businesses[0].id;
+  res.json(results.data.businesses)
+//   return axios.get(`https://api.yelp.com/v3/businesses/${id}`, config).then(results => res.json(results.data))
     })
 })
+
+router.get()
 
 
 module.exports = router
