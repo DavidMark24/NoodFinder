d5aa02dad526bd26c3c46b58aac505fe8af84984
Megan Bryan <meganmbryan12@gmail.com>
2021-04-20 21:16:08 -0700

    Add pages to choose cuisines for dinein/ eatout

--- client/src/pages/RecipeCuisines.js
+++ client/src/pages/RecipeCuisines.js
@@ -6,7 +6,7 @@ import Footer from "../components/Footer";
 import { cuisines } from "../CuisinesData";
 import { Link } from "react-router-dom";
 
-function Cuisines(props) {
+function RecipeCuisines(props) {
     const [cuisineIndex, setCuisineIndex] = useState(0);
 
     function changeCuisine(event) {
@@ -24,7 +24,7 @@ function Cuisines(props) {
                 <img src={genreLogo} width='70%' alt="Nood Finder Logo"/>
             </div>
             <div className="container logo">
-                <div className="col-md-5 col-sm-12 shadow-lg p-3 m-4 my-5 extra-rounded tan_bg">
+                <div className="col-lg-5 col-m-7 col-sm-8 shadow-lg p-3 m-4 my-5 extra-rounded tan_bg">
                     <div className="box light_brown_bg p-2 rounded">
                         <img className="rounded" src={cuisines[cuisineIndex].image} width='100%' alt=""/>
                     </div>
@@ -53,4 +53,4 @@ function Cuisines(props) {
     );
 }
 
-export default Cuisines;
+export default RecipeCuisines;
