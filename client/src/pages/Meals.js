1a45ca624d8b0ae20981b2f514fdb44cece20bfd
Ankush <ankushchalla@gmail.com>
2021-04-20 21:42:33 -0700

    Linked genre -> subgenre -> dinein.

--- client/src/pages/Meals.js
+++ client/src/pages/Meals.js
@@ -1,35 +1,44 @@
 import React from "react";
 import subgenreLogo from "../images/subgenre.svg";
 import Footer from "../components/Footer";
+import { withRouter } from "react-router";
+import { Link } from "react-router-dom";
 
 function Meals() {
+    function getRecipes(subGenre) {
+        let root = window.location.origin;
+        const urlParams = new URLSearchParams(window.location.search);
+        let genre = urlParams.get('genre');
+        window.location.href = `${root}/dinein?genre=${genre}&subGenre=${subGenre}`;
+    }
+    
     return (
         <div>
             <div className="jumbotron logo py-4">
-                <img src={subgenreLogo} height='150' alt="Nood Finder Logo"/>
+                <img src={subgenreLogo} height='150' alt="Nood Finder Logo" />
             </div>
             <div className="container">
                 <div className="row">
-                    <div onClick={event => alert("Breakfast")} className="text-center m-4 cuisine-box box extra-rounded tan_bg col-md-5 col-sm-12 white">
+                    <div onClick={() => getRecipes("breakfast")} className="text-center m-4 cuisine-box box extra-rounded tan_bg col-md-5 col-sm-12 white">
                         <h2><strong>Breakfast</strong></h2>
                     </div>
-                    <div onClick={event => alert("Brunch")} className="text-center m-4 cuisine-box box extra-rounded tan_bg col-md-5 col-sm-12 white">
+                    <div onClick={() => getRecipes("brunch")} className="text-center m-4 cuisine-box box extra-rounded tan_bg col-md-5 col-sm-12 white">
                         <h2><strong>Brunch</strong></h2>
                     </div>
                 </div>
                 <div className="row">
-                    <div onClick={event => alert("Lunch")} className="text-center m-4 cuisine-box box extra-rounded tan_bg col-md-5 col-sm-12 white">
+                    <div onClick={() => getRecipes("lunch")} className="text-center m-4 cuisine-box box extra-rounded tan_bg col-md-5 col-sm-12 white">
                         <h2><strong>Lunch</strong></h2>
                     </div>
-                    <div onClick={event => alert("Dinner")} className="text-center m-4 cuisine-box box extra-rounded tan_bg col-md-5 col-sm-12 white">
+                    <div onClick={() => getRecipes("dinner")} className="text-center m-4 cuisine-box box extra-rounded tan_bg col-md-5 col-sm-12 white">
                         <h2><strong>Dinner</strong></h2>
                     </div>
                 </div>
                 <div className="row">
-                    <div onClick={event => alert("Dessert")} className="text-center m-4 cuisine-box box extra-rounded tan_bg col-md-5 col-sm-12 white">
+                    <div onClick={() => getRecipes("dessert")} className="text-center m-4 cuisine-box box extra-rounded tan_bg col-md-5 col-sm-12 white">
                         <h2><strong>Dessert</strong></h2>
                     </div>
-                    <div onClick={event => alert("Snacks")} className="text-center m-4 cuisine-box box extra-rounded tan_bg col-md-5 col-sm-12 white">
+                    <div onClick={() => getRecipes("snacks")} className="text-center m-4 cuisine-box box extra-rounded tan_bg col-md-5 col-sm-12 white">
                         <h2><strong>Snacks</strong></h2>
                     </div>
                 </div>
