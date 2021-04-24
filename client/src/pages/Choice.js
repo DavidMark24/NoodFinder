fd0828806ab2a87a8ae8a0513dcd5af23c92e6bc
Megan Bryan <meganmbryan12@gmail.com>
2021-04-24 12:42:05 -0700

    Add hover feature to dinein/eatout options

--- client/src/pages/Choice.js
+++ client/src/pages/Choice.js
@@ -6,6 +6,7 @@ import eatOutLogo from "../images/eatOutLogo.svg";
 import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import { motion } from "framer-motion";
 
 function Choice({history}) {
     function chooseGenre(event) {
@@ -16,28 +17,28 @@ function Choice({history}) {
     return (
         <div>
             <Navbar />
-            <div class="container">
-                <div class="row logo">
-                    <div class="col-lg-5 col-md-5 col-sm-12 shadow-lg p-3 m-4 rounded tan_bg">
-                    <Link to = {"/recipecuisines"} onClick={(e) => {chooseGenre(e)}}>
-                        <div class="box light_brown_bg p-2 rounded">
-                            <img className="rounded" src={dineIn} width='100%' alt=""/>
-                        </div>
-                        <div class="logo">
-                            <img class="mt-2 py-4" src={dineInLogo} width='90%' alt=""/>
-                        </div>
-                    </Link>
-                    </div>
-                    <div class="col-lg-5 col-md-5 col-sm-12 shadow-lg p-3 m-4 rounded tan_bg">
-                    <Link to = "/restaurantcuisines">
-                        <div class="box light_brown_bg p-2 rounded">
-                            <img className="rounded" src={eatOut} width='100%' alt=""/>
-                        </div>
-                        <div class="logo">
-                            <img class="mt-2 py-4" src={eatOutLogo} width='90%' alt=""/>
-                        </div>
-                    </Link>
-                    </div>
+            <div className="container">
+                <div className="row logo mt-4">
+                    <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.8 }} className="col-lg-5 col-md-5 col-sm-12 shadow-lg p-3 m-4 rounded tan_bg">
+                        <Link to = {"/recipecuisines"} onClick={(e) => {chooseGenre(e)}}>
+                            <div className="box light_brown_bg p-2 rounded">
+                                <img classNameName="rounded" src={dineIn} width='100%' alt=""/>
+                            </div>
+                            <div className="logo">
+                                <img className="mt-2 py-4" src={dineInLogo} width='90%' alt=""/>
+                            </div>
+                        </Link>
+                    </motion.div>
+                    <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.8 }} className="col-lg-5 col-md-5 col-sm-12 shadow-lg p-3 m-4 rounded tan_bg">
+                        <Link to = "/restaurantcuisines">
+                            <div className="box light_brown_bg p-2 rounded">
+                                <img className="rounded" src={eatOut} width='100%' alt=""/>
+                            </div>
+                            <div className="logo">
+                                <img className="mt-2 py-4" src={eatOutLogo} width='90%' alt=""/>
+                            </div>
+                        </Link>
+                    </motion.div>
                 </div>
             </div>
             <Footer />
