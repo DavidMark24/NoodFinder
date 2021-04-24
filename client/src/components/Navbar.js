cd045a23b7324f8275f5190dd7cd2f7f34dcb267
Megan Bryan <meganmbryan12@gmail.com>
2021-04-24 15:16:03 -0700

    Add page to direct to saved recipes and restaurants

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,20 +3,15 @@ import TwoOs from "../images/twoOs.svg";
 import AccountIcon from "../images/accountIcon.svg";
 import { motion } from "framer-motion";
 
-function Navbar({history}) {
-    function toSavedRecipes(event) {
-        event.preventDefault();
-        history.push('/savedrecipes');
-    }
-
+function Navbar() {
     return (
         <div>
             <nav className="navbar navbar-light navbar-color">
                 <motion.a whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.8 }} className="navbar-brand mx-3 my-1" href="/choice">
                     <h1 className="white">Only <strong>F<img src={TwoOs} width="60" alt="" />ds</strong></h1>
                 </motion.a>
-                <a onClick={(e) => toSavedRecipes(e)} href="/savedrecipes">
-                    <motion.img key={AccountIcon} src={AccountIcon} whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} className="navbar-box-right mx-4" width="60" alt="" />
+                <a href="/usersave">
+                    <motion.img key={AccountIcon} src={AccountIcon} whileHover={{ scale: 1.2, rotate: [0, 360, 360]}} whileTap={{ scale: 0.8 }} className="navbar-box-right mx-4" width="60" alt="" />
                 </a>
             </nav>
         </div>
--- client/src/pages/SavedRestaurants.js
+++ client/src/pages/SavedRestaurants.js
@@ -1,5 +1,4 @@
 import React from "react";
-import onlyFoodsLogo from "../images/onlyFoodsLogo.svg";
 import SavedCard from "../components/SavedCard";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
@@ -8,12 +7,11 @@ function SavedRestaurants({history}) {
     return (
         <div>
             <Navbar history={history}/>
-            <div className="jumbotron logo py-4">
-                <img src={onlyFoodsLogo} height='150' alt="Nood Finder Logo"/>
-            </div>
-            <div className="container">
-                <h1 class="text-center">Saved Restaurants</h1>
-                <SavedCard />
+            <div className="container mt-5">
+                <h1 class="text-center mb-5">Saved Restaurants</h1>
+                <div class = "row logo mx-1">
+                    <SavedCard />
+                </div>
             </div>
             <Footer />
         </div>
